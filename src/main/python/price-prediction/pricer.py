9875bea9f4cc2440fa593c3f259de8740a06a4c1
Quinn Koster <108247939+quinnkos@users.noreply.github.com>
2025-02-09 18:55:27 -0800

    Update price prediction model

--- src/main/python/price-prediction/pricer.py
+++ src/main/python/price-prediction/pricer.py
@@ -2,72 +2,59 @@ import pandas as pd
 import numpy as np
 import sys
 import os
-
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LinearRegression
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.ensemble import GradientBoostingRegressor
-from sklearn.ensemble import VotingRegressor
-from sklearn.metrics import r2_score
+#from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor, VotingRegressor
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+import xgboost as xgb
 
 import warnings
 warnings.filterwarnings('ignore')
 
-# Prepare dataframe
-df = pd.read_csv("price-prediction/testData.csv")
-df = df[['price', 'area', 'bedrooms', 'furnishingstatus']]
-df['furnishingstatus'] = df['furnishingstatus'].apply(lambda x: x != 'unfurnished')
+df = pd.read_csv("price-prediction/house_prices.csv")
+df = df[['date', 'price', 'bedrooms', 'sqft_living', 'sqft_lot']]
 
-# Initialize variables
-X = df[['area', 'bedrooms', 'furnishingstatus']]
+# Adjust prices for inflation
+BUYING_POWER_2024 = 1085468.77 # Compared to $100,000 in 1967, per https://www.in2013dollars.com/Housing/price-inflation
+BUYING_POWER_2014 = 757570.96
+INFLATION_RATIO = (BUYING_POWER_2024 - BUYING_POWER_2014) / BUYING_POWER_2014
+df['price'] = (df['price'] + (df['price'] * INFLATION_RATIO)).astype(int)
+df = df.drop(columns='date')
+
+# Train-test split
+X = df[['bedrooms', 'sqft_living', 'sqft_lot']]
 y = df['price']
-scalar = StandardScaler()
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model = LinearRegression()
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+mean_house_price = y_test.mean()
+mse = mean_squared_error(y_test, y_pred)
+rmse = np.sqrt(mse)
+mae = mean_absolute_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+
+'''
+print(f"Mean House Price: {mean_house_price:.4f}\n"
+      f"MSE:              {mse:.4f}\n" 
+      f"RMSE:             {rmse:.4f}\n" 
+      f"MAE:              {mae:.4f}\n"
+      f"R2:               {r2:.4f}\n")
+'''
 
 
-def get_vr() -> VotingRegressor:
-
-    # Train-test split
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-    scalar.fit(X_train)
-    X_train = scalar.transform(X_train)
-    X_test = scalar.transform(X_test)
-
-    # Initialize regression techniques
-    lr = LinearRegression()
-    gb = GradientBoostingRegressor()
-    rf = RandomForestRegressor()
-
-    regressors = [("lr", lr), ("gb", gb), ("rf", rf)]
-    '''
-    for regressor in regressors:
-        scores = cross_val_score(estimator[1], X_train, y_train, scoring='r2', cv=10)
-        print(regressor[0], np.round(np.mean(scores), 2))
-    '''
-
-    vr = VotingRegressor(regressors)
-    vr.fit(X_train, y_train)
-    y_pred = vr.predict(X_test)
-    r2 = r2_score(y_test, y_pred)
-
-    if r2 < 0.5:
-        get_vr()
-
-    return vr
-
-
-def predict_price(area, bedrooms, furnishingstatus) -> int:
-    house_data = np.array([[area, bedrooms, furnishingstatus]])
-    house_data = scalar.transform(house_data)
-    price = vr.predict(house_data)
+# Predict price on unseen (input) data
+def predict_price(bedrooms, sqft_living, sqft_lot) -> int:
+    house_data = np.array([[bedrooms, sqft_living, sqft_lot]])
+    price = model.predict(house_data)
 
     return price[0]
 
 
 wd = os.getcwd()
 
-vr = get_vr()
-inputArea = int(sys.argv[1])
-inputBedrooms = int(sys.argv[2])
-inputFurnishingstatus = bool(sys.argv[3])
-print(predict_price(inputArea, inputBedrooms, inputFurnishingstatus))
+input_bedrooms = int(sys.argv[1])
+input_sqft_living = int(sys.argv[2])
+input_sqft_lot = int(sys.argv[3])
+print(predict_price(input_bedrooms, input_sqft_living, input_sqft_lot))
